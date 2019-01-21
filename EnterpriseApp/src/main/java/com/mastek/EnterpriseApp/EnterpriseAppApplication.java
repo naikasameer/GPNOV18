6e055660125c2ec13414555e0a9afe2bfeccdb9b
LiamMcdear123456 <44975086+LiamMcdear123456@users.noreply.github.com>
2019-01-21 11:40:53 +0000

    A simple method added method61078

--- EnterpriseApp/src/main/java/com/mastek/EnterpriseApp/EnterpriseAppApplication.java
+++ EnterpriseApp/src/main/java/com/mastek/EnterpriseApp/EnterpriseAppApplication.java
@@ -16,5 +16,10 @@ public class EnterpriseAppApplication {
 		System.out.println(message);
 		
 	}
+	
+	public static void method61078(String main) {
+		System.out.println("Method written by 61078");
+	}
+	
 }
 
