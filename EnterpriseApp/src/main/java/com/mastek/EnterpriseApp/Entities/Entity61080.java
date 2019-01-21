9a851d32bbce4ad4ec4b586e1c618db62a9b31d4
George61080 <44974891+George61080@users.noreply.github.com>
2019-01-21 12:10:54 +0000

    Entity 61080 created!

---
+++ EnterpriseApp/src/main/java/com/mastek/EnterpriseApp/Entities/Entity61080.java
@@ -0,0 +1,23 @@
+package com.mastek.EnterpriseApp.Entities;
+
+public class Entity61080 {
+
+		int id;
+		String name;
+		
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		
+		
+}
