da3760fa6a52bdd20055c1f364f97605e1297e52
naikasameer <sameer.naik3@mastek.com>
2019-01-21 12:10:15 +0000

    Entity Created Entity11988

--- EnterpriseApp/src/main/java/com/mastek/EnterpriseApp/EnterpriseAppApplication.java
+++ EnterpriseApp/src/main/java/com/mastek/EnterpriseApp/EnterpriseAppApplication.java
@@ -36,7 +36,6 @@ public class EnterpriseAppApplication {
 	}
 
 
-
 	public static void method61083(String main) {
 		System.out.println("Method written by 61083🐱🐱🐱");
 	}
@@ -51,13 +50,10 @@ public class EnterpriseAppApplication {
 	}
 
 
-
-
 	public static void method61086(String main) {
 		System.out.println("Method written by 61086");
 	}
-	
-	
+
 
 	public static void method61077(String main) {
 		System.out.println("Method returned by 61077");
@@ -72,9 +68,5 @@ public class EnterpriseAppApplication {
 	public static void method11988(String main) {
 		System.out.println("Method written by 11988");
 	}
-
-
-
-
 
 }
