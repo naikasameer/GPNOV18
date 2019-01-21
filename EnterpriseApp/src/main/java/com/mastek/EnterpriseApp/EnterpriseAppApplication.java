fd4d1502f781d0fb377b57be27d020606d913288
naikasameer <sameer.naik3@mastek.com>
2019-01-21 11:10:13 +0000

    Initial Project Push

---
+++ EnterpriseApp/src/main/java/com/mastek/EnterpriseApp/EnterpriseAppApplication.java
@@ -0,0 +1,14 @@
+package com.mastek.EnterpriseApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseAppApplication.class, args);
+		System.out.println("Welcome to Enterprise App");
+	}
+}
+
