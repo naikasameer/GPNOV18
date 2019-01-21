15a9ff333875ab54db0a0768b03a1731f1a7fd90
michaelwood1996 <44974781+michaelwood1996@users.noreply.github.com>
2019-01-21 11:42:53 +0000

    A simple method added method61076

--- EnterpriseApp/src/main/java/com/mastek/EnterpriseApp/EnterpriseAppApplication.java
+++ EnterpriseApp/src/main/java/com/mastek/EnterpriseApp/EnterpriseAppApplication.java
@@ -9,6 +9,17 @@ public class EnterpriseAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EnterpriseAppApplication.class, args);
 		System.out.println("Welcome to Enterprise App");
+		System.out.println("This app is shared using GIT");
+		simplePrint("Project done using GIT");
+	}
+	
+	public static void simplePrint(String message) {
+		System.out.println(message);
+	}
+	
+	public static void method61076(String main) {
+		System.out.println("Method wirrten by 61076");
+		
 	}
 }
 
