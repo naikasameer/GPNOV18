40e8a93b9365616635545a8618eaa83ec003eea6
willogrady <william.ogrady@mastek.com>
2019-01-21 12:10:25 +0000

    Entity Created Entity61086

---
+++ EnterpriseApp/src/main/java/com/mastek/EnterpriseApp/entities/Entity61086.java
@@ -0,0 +1,21 @@
+package com.mastek.EnterpriseApp.entities;
+
+public class Entity61086 {
+	
+	int id;
+	String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
