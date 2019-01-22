ce312cca268a17267f4321d696223d8c1e7d7620
joe61081 <44974861+joe61081@users.noreply.github.com>
2019-01-22 12:02:33 +0000

    add module decleration

--- enterprise-angapp/src/app/app.module.ts
+++ enterprise-angapp/src/app/app.module.ts
@@ -3,28 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Comp61081Component } from './comp61081/comp61081.component';
-=======
 import { Comp61084Component } from './comp61084/comp61084.component';
->>>>>>> 1df0a853fd3ad04f19954dc9276a5197c11098a2
-=======
 import { Comp61073Component } from './comp61073/comp61073.component';
->>>>>>> b937608f1d0ccd34b272654f35a8d32b1c0e64a3
+import { Comp61081Component } from './comp61081/comp61081.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    Comp61081Component
-=======
-    Comp61084Component
->>>>>>> 1df0a853fd3ad04f19954dc9276a5197c11098a2
-=======
+    Comp61081Component,
+    Comp61081Component,
+    Comp61084Component,
     Comp61073Component
->>>>>>> b937608f1d0ccd34b272654f35a8d32b1c0e64a3
   ],
   imports: [
     BrowserModule,
