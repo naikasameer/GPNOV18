b937608f1d0ccd34b272654f35a8d32b1c0e64a3
61073 <mike.wood@mastek.com>
2019-01-22 11:57:46 +0000

    Added component

--- enterprise-angapp/src/app/app.module.ts
+++ enterprise-angapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Comp61073Component } from './comp61073/comp61073.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp61073Component
   ],
   imports: [
     BrowserModule,
