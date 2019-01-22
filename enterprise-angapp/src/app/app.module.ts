95ac2786f7dcd821b4267dc929d2109e6be52ae3
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-01-22 11:59:04 +0000

    new 61082 component added

--- enterprise-angapp/src/app/app.module.ts
+++ enterprise-angapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Comp61082Component } from './comp61082/comp61082.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp61082Component
   ],
   imports: [
     BrowserModule,
