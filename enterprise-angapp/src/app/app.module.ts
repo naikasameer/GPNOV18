f3633a8a581a7c45f93d7f6539b8e723ebdf731f
LiamMcdear123456 <44975086+LiamMcdear123456@users.noreply.github.com>
2019-01-22 11:59:42 +0000

    1

--- enterprise-angapp/src/app/app.module.ts
+++ enterprise-angapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Comp61078Component } from './comp61078/comp61078.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp61078Component
   ],
   imports: [
     BrowserModule,
