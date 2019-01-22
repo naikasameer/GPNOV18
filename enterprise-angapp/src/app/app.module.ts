e4dfaf0285c4a10ce8f15706e7b8d3150c80c213
willogrady <william.ogrady@mastek.com>
2019-01-22 12:12:26 +0000

    comp61086 added

--- enterprise-angapp/src/app/app.module.ts
+++ enterprise-angapp/src/app/app.module.ts
@@ -3,61 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Comp61086Component } from './comp61086/comp61086.component';
-=======
 import { Comp61078Component } from './comp61078/comp61078.component';
->>>>>>> 8713d6181353bf134edddb8be660f747981d369e
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+
+
 import { Comp61081Component } from './comp61081/comp61081.component';
-=======
->>>>>>> b622912368f1e491afc65dfc682fc333bb9055f0
-=======
->>>>>>> ccd07b32c8621eb85e0eed1037a31a1827fe7a4e
+
+
 import { Comp61084Component } from './comp61084/comp61084.component';
 import { Comp61073Component } from './comp61073/comp61073.component';
-<<<<<<< HEAD
->>>>>>> b937608f1d0ccd34b272654f35a8d32b1c0e64a3
->>>>>>> 1df0a853fd3ad04f19954dc9276a5197c11098a2
-=======
-import { Comp61081Component } from './comp61081/comp61081.component';
->>>>>>> ccd07b32c8621eb85e0eed1037a31a1827fe7a4e
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    Comp61086Component
-=======
-    Comp61078Component
->>>>>>> 8713d6181353bf134edddb8be660f747981d369e
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-    Comp61081Component
-=======
->>>>>>> b622912368f1e491afc65dfc682fc333bb9055f0
-    Comp61084Component
->>>>>>> 1df0a853fd3ad04f19954dc9276a5197c11098a2
-=======
-    Comp61073Component
->>>>>>> b937608f1d0ccd34b272654f35a8d32b1c0e64a3
->>>>>>> 1df0a853fd3ad04f19954dc9276a5197c11098a2
-=======
+
+    Comp61086Component,
+
+    Comp61078Component,
+
+    Comp61081Component,
+
+    Comp61084Component,
+
+    Comp61073Component,
+
     Comp61081Component,
     Comp61084Component,
     Comp61073Component
->>>>>>> ccd07b32c8621eb85e0eed1037a31a1827fe7a4e
+
   ],
   imports: [
     BrowserModule,
