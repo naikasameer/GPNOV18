ccd07b32c8621eb85e0eed1037a31a1827fe7a4e
joe61081 <44974861+joe61081@users.noreply.github.com>
2019-01-22 12:02:58 +0000

    ...

--- enterprise-angapp/src/app/app.module.ts
+++ enterprise-angapp/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Comp61081Component } from './comp61081/comp61081.component';
 import { Comp61084Component } from './comp61084/comp61084.component';
 import { Comp61073Component } from './comp61073/comp61073.component';
 import { Comp61081Component } from './comp61081/comp61081.component';
@@ -11,7 +10,6 @@ import { Comp61081Component } from './comp61081/comp61081.component';
 @NgModule({
   declarations: [
     AppComponent,
-    Comp61081Component,
     Comp61081Component,
     Comp61084Component,
     Comp61073Component
