1665747aadecc9d5afa98487b5be6419181840cd
scottfreeman1996 <Scott61079@Mastek.Com>
2019-01-22 12:19:34 +0000

    c

--- enterprise-angapp/src/app/app.module.ts
+++ enterprise-angapp/src/app/app.module.ts
@@ -2,55 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { Comp61079Component } from './comp61079/comp61079.component';
-=======
-import { Comp61081Component } from './comp61081/comp61081.component';
->>>>>>> b622912368f1e491afc65dfc682fc333bb9055f0
-=======
-=======
-import { Comp61086Component } from './comp61086/comp61086.component';
->>>>>>> 7c9f771f980656b7f3ecd478b817dffb91ad05c6
-=======
-=======
 
+import { Comp61086Component } from './comp61086/comp61086.component';
 import { Comp61072Component } from './comp61072/comp61072.component';
 import { Comp61081Component } from './comp61081/comp61081.component';
-import { Comp61084Component } from './comp61084/comp61084.component';
-import { Comp61073Component } from './comp61073/comp61073.component';
-import { Comp61086Component } from './comp61086/comp61086.component';
+
 import { Comp61078Component } from './comp61078/comp61078.component';
 
 
 
->>>>>>> e825fb358b1f2faa2cb3d4468cd4e01b438d064b
-=======
-import { Comp61086Component } from './comp61086/comp61086.component';
-
->>>>>>> 5c85d18b54ec9b91cb3bf0be8a6c966a4862e75a
-import { Comp61078Component } from './comp61078/comp61078.component';
 
 
-import { Comp61081Component } from './comp61081/comp61081.component';
-<<<<<<< HEAD
-=======
->>>>>>> b622912368f1e491afc65dfc682fc333bb9055f0
->>>>>>> 8713d6181353bf134edddb8be660f747981d369e
-=======
->>>>>>> ccd07b32c8621eb85e0eed1037a31a1827fe7a4e
-=======
-
->>>>>>> 5c85d18b54ec9b91cb3bf0be8a6c966a4862e75a
 import { Comp61084Component } from './comp61084/comp61084.component';
 import { Comp61073Component } from './comp61073/comp61073.component';
->>>>>>> 626dd84f1d3bee02b76f7f3c6849049ecfd60ab4
+
 
 
 
@@ -59,49 +25,10 @@ import { Comp61073Component } from './comp61073/comp61073.component';
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    Comp61079Component
-=======
-    Comp61081Component
->>>>>>> b622912368f1e491afc65dfc682fc333bb9055f0
-=======
-    Comp61084Component
->>>>>>> 1df0a853fd3ad04f19954dc9276a5197c11098a2
-=======
-=======
-    Comp61086Component
->>>>>>> 7c9f771f980656b7f3ecd478b817dffb91ad05c6
-=======
-    Comp61078Component
->>>>>>> 8713d6181353bf134edddb8be660f747981d369e
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-    Comp61081Component
-=======
->>>>>>> b622912368f1e491afc65dfc682fc333bb9055f0
-    Comp61084Component
->>>>>>> 1df0a853fd3ad04f19954dc9276a5197c11098a2
-=======
-    Comp61073Component
->>>>>>> b937608f1d0ccd34b272654f35a8d32b1c0e64a3
->>>>>>> 1df0a853fd3ad04f19954dc9276a5197c11098a2
->>>>>>> 8713d6181353bf134edddb8be660f747981d369e
-=======
+
     Comp61081Component,
-    Comp61084Component,
->>>>>>> ccd07b32c8621eb85e0eed1037a31a1827fe7a4e
-    Comp61073Component
->>>>>>> ccd07b32c8621eb85e0eed1037a31a1827fe7a4e
-=======
+
+   
     Comp61072Component,
     Comp61081Component,
     Comp61084Component,
@@ -110,10 +37,6 @@ import { Comp61073Component } from './comp61073/comp61073.component';
     Comp61073Component,
  
   
-
->>>>>>> e825fb358b1f2faa2cb3d4468cd4e01b438d064b
-=======
-
     Comp61086Component,
 
     Comp61078Component,
@@ -123,9 +46,6 @@ import { Comp61073Component } from './comp61073/comp61073.component';
     Comp61073Component,
     Comp61084Component,
     Comp61073Component,
->>>>>>> 626dd84f1d3bee02b76f7f3c6849049ecfd60ab4
-
->>>>>>> 5c85d18b54ec9b91cb3bf0be8a6c966a4862e75a
   ],
   imports: [
     BrowserModule,
